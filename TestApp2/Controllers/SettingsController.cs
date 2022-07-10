7777dd477cabeab4dbed3dd20f4e26723ca91cea
Anton Oreshko <oreshko2010@gmail.com>
2022-07-10 22:31:38 +0300

    init

---
+++ TestApp2/Controllers/SettingsController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TestApp2.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class SettingsController: ControllerBase
+{
+    private readonly IConfiguration _config;
+    
+    public SettingsController(IConfiguration config)
+    {
+        _config = config;
+    }
+
+    [HttpGet]
+    public IActionResult GetSetting(string settingPath)
+    {
+        var value = _config[settingPath];
+        if (value == null)
+            return NotFound();
+
+        return Ok(value);
+    }
+}
---
+++ TestApp2/Program.cs
@@ -0,0 +1,42 @@
+using Amazon.Extensions.NETCore.Setup;
+using Amazon.Runtime;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Configuration.AddSystemsManager("/test-app-2/Development/", reloadAfter: TimeSpan.FromMinutes(5));
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// way 1
+builder.Services.AddDefaultAWSOptions(builder.Configuration.GetAWSOptions());
+// way2
+// var awsOptions = builder.Configuration.GetAWSOptions();
+// builder.Services.AddDefaultAWSOptions(awsOptions);
+// way3
+// var awsOptions = new AWSOptions 
+// {
+//     Credentials = new BasicAWSCredentials("yourAccessKey", "yourAccessSecret")
+// };
+// builder.Services.AddDefaultAWSOptions(awsOptions);
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+/**/
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
