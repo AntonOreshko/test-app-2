aed0f855aec62e1f79f897f3c6136a4b9aef25f4
Anton Oreshko <oreshko2010@gmail.com>
2022-07-11 21:32:09 +0300

    fix settings

--- TestApp2/Program.cs
+++ TestApp2/Program.cs
@@ -1,29 +1,31 @@
+using Amazon;
 using Amazon.Extensions.NETCore.Setup;
 using Amazon.Runtime;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Configuration.AddSystemsManager("/test-app-2/Development/", reloadAfter: TimeSpan.FromMinutes(5));
+var accessKey = Environment.GetEnvironmentVariable("AWS_ACCESS_KEY_ID");
+var accessSecret = Environment.GetEnvironmentVariable("AWS_SECRET_ACCESS_KEY");
+var region = Environment.GetEnvironmentVariable("AWS_DEFAULT_REGION");
+var environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+var awsOptions = new AWSOptions
+{
+    Credentials = new BasicAWSCredentials(accessKey, accessSecret),
+    Region = RegionEndpoint.GetBySystemName(region)
+};
 
-// Add services to the container.
+var path = $"/test-app-2/{environment}/";
+builder.Configuration.AddSystemsManager(path, reloadAfter: TimeSpan.FromMinutes(5), awsOptions: awsOptions);
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddHealthChecks();
 
-// way 1
-//builder.Services.AddDefaultAWSOptions();
-// way2
-// var awsOptions = builder.Configuration.GetAWSOptions();
-// builder.Services.AddDefaultAWSOptions(awsOptions);
-// way3
-// var awsOptions = new AWSOptions 
-// {
-//     Credentials = new BasicAWSCredentials("yourAccessKey", "yourAccessSecret")
-// };
-// builder.Services.AddDefaultAWSOptions(awsOptions);
+builder.Services.AddDefaultAWSOptions(awsOptions);
+
+//var awsOptions = builder.Configuration.GetAWSOptions();
+//builder.Services.AddDefaultAWSOptions(awsOptions);
 
 var app = builder.Build();
 
@@ -32,7 +34,6 @@ app.MapHealthChecks("/health");
 app.UseSwagger();
 app.UseSwaggerUI();
 
-/**/
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
